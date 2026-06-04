ebd78e066df5595fee1fd0b078aa2c456a669d30
perandber <perandber@alu.edu.gva.es>
2026-06-04 10:08:42 +0200

    Error al intentar iniciar BAckend

--- backend/backend/src/negocios/negocios.controller.ts
+++ backend/backend/src/negocios/negocios.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpCo
 import { NegociosService } from './negocios.service';
 import { CreateNegociosDto } from './dto/create-negocios.dto';
 import { UpdateNegociosDto } from './dto/update-negocios.dto';
-import { LoginNegociosDto } from './dto/login-negocios.dto';
+
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
@@ -35,18 +35,7 @@ export class NegociosController {
     return this.negociosService.create(createNegociosDto);
   }
 
-  @Post('register')
-  @ApiCreatedResponse({ description: 'Negocio registrado con contraseña' })
-  register(@Body() createNegociosDto: CreateNegociosDto) {
-    return this.negociosService.register(createNegociosDto);
-  }
 
-  @Post('login')
-  @HttpCode(200)
-  @ApiOkResponse({ description: 'Login correcto, devuelve el negocio' })
-  login(@Body() loginDto: LoginNegociosDto) {
-    return this.negociosService.login(loginDto.Telefono, loginDto.password);
-  }
 
   @Get()
   @ApiOkResponse({ description: 'Listado de negocios' })
